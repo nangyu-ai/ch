1be7a7cb2a5400f357ddf390b8b03655e921a296
nangyu-ai <nangyu@gmail.com>
2025-09-03 10:57:31 +0900

    Add files via upload

--- test/io/FilePathNameList.java
+++ test/io/FilePathNameList.java
@@ -14,5 +14,221 @@ public class FilePathNameList {
 		for (int i = 0; i < files.length; i++) {
 		    System.out.println("file: " + files[i]);
 		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
 	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
+}
+package test.io;
+
+import java.io.File;
+
+public class FilePathNameList {
+	public static void main(String[] args) {
+		new FilePathNameList();
+	}
+	
+	public FilePathNameList() {
+		File dir = new File("D:/dhjang/ch/playground/eco-playground.kr/safe/upload_document_safety");
+		File files[] = dir.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+		    System.out.println("file: " + files[i]);
+		}
+	}aaaaaa
 }
